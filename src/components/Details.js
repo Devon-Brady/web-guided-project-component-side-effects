ad7ee1a8f148390056d3623c0762837d865a06eb
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-07 19:57:30 +0200

    wip

--- src/components/Details.js
+++ src/components/Details.js
@@ -43,6 +43,10 @@ export default function Details(props) {
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+  useEffect(() => {
+    // this runs after first render for sure, and then after every render+dom
+  }, [friendId])
+
   console.log('***** RENDERING! *****')
 
   return (
